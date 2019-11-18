9f459b13d6da78233b0cfefa6fc94918c347ed1f
Pedro García <pedro.garcia@zentagroup.com>
2019-11-18 08:03:24 -0300

    change test

--- src/test/java/com/pharmacy/turn/config/WSConfigTest.java
+++ src/test/java/com/pharmacy/turn/config/WSConfigTest.java
@@ -12,30 +12,30 @@ public class WSConfigTest {
     public static final String MUST_BE_A_NOTNULL = "can't be a null value";
     public static final String ANY_STRING = "";
 
-    private WSConfig avlConfig;
+    private WSConfig config;
 
     @Before
     public void before() {
 
-        avlConfig = new WSConfig(ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING);
+        config = new WSConfig(ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING, ANY_STRING);
 
-        avlConfig.setPharmacyEndpoint(ANY_STRING);
-        avlConfig.setPharmacyProcess(ANY_STRING);
-        avlConfig.setPharmacyScheme(ANY_STRING);
-        avlConfig.setCommuneEndpoint(ANY_STRING);
-        avlConfig.setCommuneProcess(ANY_STRING);
-        avlConfig.setCommuneScheme(ANY_STRING);
+        config.setPharmacyEndpoint(ANY_STRING);
+        config.setPharmacyProcess(ANY_STRING);
+        config.setPharmacyScheme(ANY_STRING);
+        config.setCommuneEndpoint(ANY_STRING);
+        config.setCommuneProcess(ANY_STRING);
+        config.setCommuneScheme(ANY_STRING);
 
     }
 
     @Test
     public void testEnviromentValues() {
-        Assert.assertNotNull(MUST_BE_A_NOTNULL, avlConfig);
-        Assert.assertNotNull(MUST_BE_A_NOTNULL, avlConfig.getPharmacyEndpoint());
-        Assert.assertNotNull(MUST_BE_A_NOTNULL, avlConfig.getPharmacyProcess());
-        Assert.assertNotNull(MUST_BE_A_NOTNULL, avlConfig.getPharmacyScheme());
-        Assert.assertNotNull(MUST_BE_A_NOTNULL, avlConfig.getCommuneEndpoint());
-        Assert.assertNotNull(MUST_BE_A_NOTNULL, avlConfig.getCommuneProcess());
-        Assert.assertNotNull(MUST_BE_A_NOTNULL, avlConfig.getCommuneScheme());
+        Assert.assertNotNull(MUST_BE_A_NOTNULL, config);
+        Assert.assertNotNull(MUST_BE_A_NOTNULL, config.getPharmacyEndpoint());
+        Assert.assertNotNull(MUST_BE_A_NOTNULL, config.getPharmacyProcess());
+        Assert.assertNotNull(MUST_BE_A_NOTNULL, config.getPharmacyScheme());
+        Assert.assertNotNull(MUST_BE_A_NOTNULL, config.getCommuneEndpoint());
+        Assert.assertNotNull(MUST_BE_A_NOTNULL, config.getCommuneProcess());
+        Assert.assertNotNull(MUST_BE_A_NOTNULL, config.getCommuneScheme());
     }
 }
